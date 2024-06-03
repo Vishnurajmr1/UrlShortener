5b7d5c9c690a366905bd7eeabb24cea9b7c5b27e
Vishnurajmr1 <vishnurajmr1@gmail.com>
2024-06-03 22:02:10 +0530

    bug:change ngif error messages

--- client/src/app/components/url-form/url-form.component.ts
+++ client/src/app/components/url-form/url-form.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { InputIconModule } from 'primeng/inputicon';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
@@ -20,15 +28,38 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
     InputGroupModule,
     InputGroupAddonModule,
     InputTextareaModule,
+    ReactiveFormsModule,
+    CommonModule,
   ],
   templateUrl: './url-form.component.html',
   styleUrl: './url-form.component.scss',
 })
 export class UrlFormComponent {
+  urlForm!: FormGroup;
+  private fb = inject(FormBuilder);
   urlValue: string = '';
+  ngOnInit(): void {
+    this.urlForm = this.fb.group({
+      url: ['', [Validators.required, this.urlValidator]],
+    });
+  }
 
-  onSubmit(){
-    console.log(this.urlValue)
-    this.urlValue=''
+  urlValidator(control: AbstractControl) {
+    const urlPattern =
+    /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+    return urlPattern.test(control.value) ? null : { invalidUrl: true };
+  }
+  onSubmit() {
+    if (this.urlForm.valid) {
+      const urlValue = this.urlForm.get('url')?.value;
+      console.log('Submitted URL:', urlValue);
+      this.urlForm.reset();
+    } else {
+      this.urlForm.markAllAsTouched();
+    }
+  }
+
+  get urlControl() {
+    return this.urlForm.get('url');
   }
 }
--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -1,12 +1,16 @@
 import { Injectable, inject } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  private baseUrl=environment.baseUrl;
-  private http=inject(HttpClient);
-  
+  private baseUrl = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  createUrl(url: string): Observable<string> {
+    return this.http.post<string>(`${this.baseUrl}/url`, url);
+  }
 }
